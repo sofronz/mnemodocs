cf004d33fb4542e43b76aa51356de6dddac31689
Sofronius Ruddy <ruddy@circlecreative.dev>
2025-04-16 19:38:28 +0700

    docs(model): update dataExport

--- app/Models/DataExport.php
+++ app/Models/DataExport.php
@@ -7,12 +7,28 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 /**
  *
  *
+ * @property string $id
+ * @property string $type
+ * @property string|null $file_path
+ * @property string $exported_by
+ * @property object|null $metadata
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property \Illuminate\Support\Carbon|null $deleted_at
  * @property-read \Illuminate\Database\Eloquent\Collection<int, \OwenIt\Auditing\Models\Audit> $audits
  * @property-read int|null $audits_count
  * @method static \Illuminate\Database\Eloquent\Builder<static>|DataExport newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder<static>|DataExport newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder<static>|DataExport onlyTrashed()
  * @method static \Illuminate\Database\Eloquent\Builder<static>|DataExport query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|DataExport whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|DataExport whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|DataExport whereExportedBy($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|DataExport whereFilePath($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|DataExport whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|DataExport whereMetadata($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|DataExport whereType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|DataExport whereUpdatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder<static>|DataExport withTrashed()
  * @method static \Illuminate\Database\Eloquent\Builder<static>|DataExport withoutTrashed()
  * @mixin \Eloquent
